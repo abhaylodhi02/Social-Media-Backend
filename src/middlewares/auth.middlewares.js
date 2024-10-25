275430a0d0f75a031802a4ee22c8542c2cfb01ca
abhaylodhi02 <abhaylodhi108@gmail.com>
2024-10-25 12:05:35 +0530

    bug fix

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -7,17 +7,17 @@ import { User } from "../models/user.model.js";
 export const verifyJWT = asyncHandler(async(req, res, next) => {
   try {
       const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
-      console.log('Token:', token); // Log the token for debugging
+      //console.log('Token:', token); // Log the token for debugging
 
       if (!token) {
           throw new ApiError(401, "Unauthorized request!");
       }
 
       const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-      console.log('Decoded Token:', decodedToken); // Log decoded token
+      //console.log('Decoded Token:', decodedToken); // Log decoded token
 
       const user = await User.findById(decodedToken?._id).select("-password -refreshToken");
-      console.log('User:', user); // Log user
+     // console.log('User:', user); // Log user
 
       if (!user) {
           throw new ApiError(401, "Invalid Access Token!");
